a318cd5cb36dfe949ba0f9ec0bd22efc56668485
Zakjanzi <zakjanzi97@gmail.com>
2023-01-09 18:50:24 +0200

    auth hook

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,37 @@ import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
 import { UserContext } from '../lib/context';
 
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../lib/firebase';
+import { Firestore } from 'firebase/compat/firestore';
+
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null)
+
+  useEffect(() => {
+    // turn off realtime subscription
+    let unsubscribe;
+
+    if (user) {
+      const ref = firestore.collection('users').doc(user.uid)
+      unsubscribe = ref.onSnapshot((doc) => {
+        setUsername(doc.data()?.username);
+      });
+    } else {
+      setUsername(null);
+    }
+
+    return unsubscribe
+
+  }, [user])
+
+
   return (
 
-    <UserContext.Provider value={{ user:{}, username:'zak' }}>
+    <UserContext.Provider value={{ user, username }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
