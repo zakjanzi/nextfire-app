02951ff7da4f58e83684563c2020829537bdd06a
Zakjanzi <zakjanzi97@gmail.com>
2023-01-09 18:02:52 +0200

    added sign in w/ google button

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,46 @@
-export default function EnterPage({ }) {
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+    const user = null;
+    const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+
     return (
-        <main>
-            <h1>Sign Up</h1>
-        </main>
-    )
+    <main>
+      
+      {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}
+    </main>
+    );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    //the below line triggers a popup in the user's window
+    await auth.signInWithPopup(googleAuthProvider);
+    // ideally, you would wrap this with a try/catch block ^.
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  )
+
+}
+
+// Sign out button
+function SignOutButton() {
+  return (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+
+}
+
+
+function UsernameForm() {
+
 }
